206410de665f9b013ecb499f11ebdb514049b70d
AnupJoseph <anup20joseph@gmail.com>
2021-05-13 05:48:05 +0530

    Web app done

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from re import sub
 import streamlit as st
 from defaults import DEFAULT_TEXT
-from nlp_scripts import initialize_entity_model, initialize_relations_model, extracts_entities
+from nlp_scripts import initialize_entity_model, initialize_relations_model, extracts_entities, classify_relations
 
 # status_text = st.empty()
 # status_text.text()
@@ -11,9 +11,9 @@ with st.spinner("Loading Entity Recognition Model..."):
     nlp = initialize_entity_model()
 st.success("finished loading NER module")
 
-# with st.spinner("Loading Entity Recognition Model..."):
-#     relations = initialize_relations_model()
-# st.success("finished loading NER module")
+with st.spinner("Loading Relation Categorization Model..."):
+    relations = initialize_relations_model()
+st.success("finished loading REL module")
 
 
 st.title("Joint Entities and Relation Extractor")
@@ -24,5 +24,17 @@ with st.form(key='my_form'):
     submit_button = st.form_submit_button(label='Submit')
 
 if submit_button:
-    entities = extracts_entities(nlp,text_input)
+    entities, doc = extracts_entities(nlp, text_input)
     st.dataframe(entities)
+
+
+with st.form(key='my_form'):
+    threshold = st.slider(label="Choose a threshold for the model",
+                          min_value=0.5, max_value=1.0, step=0.01)
+    chosen_relation = st.selectbox("Choose the relation you want to explore", [
+                                   "EXPERIENCE_IN", "DEGREE_IN"])
+    rel_submit_button = st.form_submit_button(label='Explore the relation')
+
+if rel_submit_button:
+    df = classify_relations(st, relations, doc, chosen_relation, threshold)
+    st.dataframe(df)
--- nlp_scripts.py
+++ nlp_scripts.py
@@ -1,5 +1,6 @@
 import spacy
 from pandas import DataFrame
+# import streamlit as st
 
 
 def initialize_entity_model(path="/content/model-best/"):
@@ -15,9 +16,38 @@ def initialize_relations_model(path="/content/rel_model/model-best"):
 
 def extracts_entities(nlp, text):
     # entity_lists = []
-    text=[text]
+    text = [text]
     for doc in nlp.pipe(text, disable=["tagger"]):
         e_list = [(e.start, e.text, e.label_) for e in doc.ents]
     df = DataFrame(e_list)
-    df.columns = ["Position","Text","Label"]
+    df.columns = ["Position", "Text", "Label"]
+    return df, doc
+
+
+def classify_relations(st, relation_model, doc, chosen_relation, threshold):
+    st.text("Starting processing individual docs")
+    process_bar = st.progress(0)
+    percent_complete = 0
+    for _, proc in relation_model.pipeline:
+        doc = proc(doc)
+        process_bar.progress(percent_complete + 1)
+        percent_complete += 1
+    st.text("Finished processing docs")
+
+    process_bar = st.progress(0)
+    percent_complete = 0
+    relations = []
+    for value, rel_dict in doc._.rel.items():
+        for sent in doc.sents:
+            for e in sent.ents:
+                process_bar.progress(percent_complete + 1)
+                percent_complete += 1
+                for b in sent.ents:
+                    if e.start == value[0] and b.start == value[1]:
+                        if rel_dict[chosen_relation] >= threshold:
+                            # print(
+                            #     f" entities: {e.text, b.text} --> predicted relation: {rel_dict}")
+                            relations.append([e.text,b.text,rel_dict.values()])
+    df = DataFrame(relations,columns=["First Entity, Second Entity, Conf. DEGREE_IN, Conf. EXPERIENCE_IN"])
     return df
+                            
